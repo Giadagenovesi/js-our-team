99b4b4b95b2d7d31dc45c76dc2493818f2892485
Giadagenovesi <giada.g@outlook.it>
2023-03-22 13:13:37 +0100

    Add project scaffolding

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Creare l’array di oggetti con le informazioni dei diversi membri del team.
+
+
+
+//Stampare su console le informazioni di nome, ruolo e la stringa della foto 
+
+//Stampare le stesse informazioni su DOM sottoforma di stringhe
